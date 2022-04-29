4d95a49355fdf6ed79dbe65231565569b3924bd2
Lucas Nhimi <lucasnhimi@gmail.com>
2022-04-29 16:22:46 -0300

    Adding tailwind css

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,7 @@
-import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return (
+    <button type='button' className='bg-green-500 hover:bg-green-700 h-16 w-16'>
+      Boop
+    </button>
+  );
 };
